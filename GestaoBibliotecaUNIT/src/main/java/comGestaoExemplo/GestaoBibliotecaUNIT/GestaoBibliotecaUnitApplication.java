cdfd120ea377a11d5b02bbc057606f7c9c330cf0
Matheus Vinicius da Silva Alcantara <1212309674@acd.grupotiradentes.com>
2022-05-26 19:53:36 -0300

    projeto base criado

---
+++ GestaoBibliotecaUNIT/src/main/java/comGestaoExemplo/GestaoBibliotecaUNIT/GestaoBibliotecaUnitApplication.java
@@ -0,0 +1,13 @@
+package comGestaoExemplo.GestaoBibliotecaUNIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoBibliotecaUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoBibliotecaUnitApplication.class, args);
+	}
+
+}
---
+++ GestaoBibliotecaUNIT/src/test/java/comGestaoExemplo/GestaoBibliotecaUNIT/GestaoBibliotecaUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package comGestaoExemplo.GestaoBibliotecaUNIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoBibliotecaUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
