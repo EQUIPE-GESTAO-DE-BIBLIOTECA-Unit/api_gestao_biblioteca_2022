d967d8c3befbe8d16211696c667fc3bae8a9d2b1
fernandobituco <fernandoangelima@gmail.com>
2022-06-16 22:30:50 -0300

    put e delete obra adicionados

--- GestaoBibliotecaUNIT/src/main/java/comGestaoExemplo/GestaoBibliotecaUNIT/controller/ObraController.java
+++ GestaoBibliotecaUNIT/src/main/java/comGestaoExemplo/GestaoBibliotecaUNIT/controller/ObraController.java
@@ -3,8 +3,10 @@ package comGestaoExemplo.GestaoBibliotecaUNIT.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +28,16 @@ public class ObraController {
     @PostMapping
     public Obra save(@RequestBody Obra novaObra){
         return obraRepo.save(novaObra);
+    }
+
+    @PutMapping
+    public Obra update(@RequestBody Obra obraAtualizar){
+        return obraRepo.save(obraAtualizar);
+    }
+
+    @DeleteMapping
+    public String delete(@RequestBody Obra obraDeletar){
+        obraRepo.delete(obraDeletar);
+        return "usuário deletado com sucesso!";
     }
 }
